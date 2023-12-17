5471b89c5e1c33d45133bf0e2ba04a5af125407c
Cordeva <ahmed.abdelaaall@gmail.com>
2023-12-17 15:33:47 +0200

    react strict mode off, to make the calendar buttons work

--- src/components/TheCalendar/TheCalendar.tsx
+++ src/components/TheCalendar/TheCalendar.tsx
@@ -1,49 +1,80 @@
 "use client"
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-
 const locales = {
-    "en-US": require("date-fns/locale/en-US"),
-};
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-});
-
-const events = [
-    {
-        title: "Big Meeting",
-        start: new Date(2023, 12, 20),
-        end: new Date(2023, 12, 22),
-    },
-    {
-        title: "Vacation",
-        start: new Date(2021, 6, 7),
-        end: new Date(2021, 6, 10),
-    },
-    {
-        title: "Conference",
-        start: new Date(2021, 6, 20),
-        end: new Date(2021, 6, 23),
-    },
-];
-
-function TheCalendar() {
-    
-
-    return (
-        <div className="calendar">
-            <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style={{ height: 800, margin: "50px" }} />
-        </div>
-    );
+  'en-US': enUS,
 }
 
-export default TheCalendar;
+const events = [
+  {
+      title: "Big Meeting",
+      start: new Date(2023, 11, 10),
+      end: new Date(2023, 11, 10),
+  },
+  {
+      title: "Big Meeting",
+      start: new Date(2023, 11, 10),
+      end: new Date(2023, 11, 10),
+  },
+  {
+      title: "Big Meeting",
+      start: new Date(2023, 11, 10),
+      end: new Date(2023, 11, 10),
+  },
+  {
+      title: "Big Meeting",
+      start: new Date(2023, 11, 10),
+      end: new Date(2023, 11, 10),
+  },
+  {
+      title: "Big Meeting",
+      start: new Date(2023, 11, 10),
+      end: new Date(2023, 11, 10),
+  },
+  {
+      title: "Big Meeting",
+      start: new Date(2023, 11, 10),
+      end: new Date(2023, 11, 10),
+  },
+  {
+      title: "Big Meeting",
+      start: new Date(2023, 11, 10),
+      end: new Date(2023, 11, 10),
+  },
+  {
+      title: "Vacation",
+      start: new Date(2021, 6, 7),
+      end: new Date(2021, 6, 10),
+  },
+  {
+      title: "Conference",
+      start: new Date(2021, 6, 20),
+      end: new Date(2021, 6, 23),
+  },
+];
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const MyCalendar = (props) => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 1000 }}
+    />
+  </div>
+)
+export default MyCalendar;
