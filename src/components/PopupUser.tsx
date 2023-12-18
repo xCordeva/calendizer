f289e911a30aa04d4049bface3eda032dbcd9a39
Cordeva <ahmed.abdelaaall@gmail.com>
2023-12-18 23:35:00 +0200

    made sign out functionality

--- src/components/PopupUser.tsx
+++ src/components/PopupUser.tsx
@@ -1,12 +1,33 @@
-import Link from "next/link";
+import { useSignOut } from 'react-firebase-hooks/auth'
+import {auth} from '../firebase/firebaseConfig'
+import { useDispatch } from "react-redux";
+import { closeUserPopup } from '../features/UserPopup';
+import { useRouter } from 'next/navigation';
+
 
 export default function PopupUser() {
+
+  const dispatch = useDispatch()
+  const [signOut] = useSignOut(auth);
+  const router = useRouter()
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      // close popup after sign out
+      dispatch(closeUserPopup(false));
+      // send user to sign in page after sign out
+      router.push('/sign-in')
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
   return (
     <div className="popup">
       <div className="popup-user">
         <h4>Cordeva Cordeva Cordeva</h4>
         <hr />
-        <Link href='/'>Sign Out</Link>
+        <button onClick={handleSignOut}>Sign Out</button>
         </div>
     </div>
   )
