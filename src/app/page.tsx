b625abcd5d4d7310331c16d84200697db512d5f2
Cordeva <ahmed.abdelaaall@gmail.com>
2023-12-26 21:52:17 +0200

    removed unused Auth component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
-// pages/index.js
-'use client'
-import Navbar from '@/components/Navbar';
-import  '@/css/global.css'
-import usePopupCloser from '@/Custom Hooks/usePopupCloser'
-import Sidebar from '@/components/Sidebar'
+"use client";
+import Navbar from "@/components/Navbar";
+import "@/css/global.css";
+import usePopupCloser from "@/Custom Hooks/usePopupCloser";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
-
-  usePopupCloser()
+  usePopupCloser();
 
   return (
     <div>
-      <Navbar/>
-      <Sidebar/>
+      <Navbar />
+      <Sidebar />
     </div>
   );
 }
