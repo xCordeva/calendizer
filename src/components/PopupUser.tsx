2d7c318ed2fd97a199ad908c639e13242851db02
Cordeva <ahmed.abdelaaall@gmail.com>
2023-12-29 03:20:50 +0200

    added the Name of the user to the user popup

--- src/components/PopupUser.tsx
+++ src/components/PopupUser.tsx
@@ -1,34 +1,34 @@
-import { useSignOut } from 'react-firebase-hooks/auth'
-import {auth} from '../firebase/firebaseConfig'
+import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/firebaseConfig";
 import { useDispatch } from "react-redux";
-import { closeUserPopup } from '../features/UserPopup';
-import { useRouter } from 'next/navigation';
-
+import { closeUserPopup } from "../features/UserPopup";
+import { useRouter } from "next/navigation";
+import useAuth from "@/Custom Hooks/useAuth";
 
 export default function PopupUser() {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [signOut] = useSignOut(auth);
-  const router = useRouter()
+  const router = useRouter();
   const handleSignOut = async () => {
     try {
       await signOut();
       // close popup after sign out
       dispatch(closeUserPopup(false));
       // send user to sign in page after sign out
-      router.push('/sign-in')
+      router.push("/sign-in");
     } catch (error) {
-      console.error('Error signing out:', error);
+      console.error("Error signing out:", error);
     }
   };
+  const { user } = useAuth();
 
   return (
     <div className="popup">
       <div className="popup-user">
-        <h4>Cordeva Cordeva Cordeva</h4>
+        <h4>{user && user.displayName}</h4>
         <hr />
         <button onClick={handleSignOut}>Sign Out</button>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
