af9620410701f03dc33a0c48ec8be926167c4207
Cordeva <ahmed.abdelaaall@gmail.com>
2024-01-01 03:53:49 +0200

    update package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../css/global.css";
 import { ReduxProvider } from "@/features/provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Calendizer | Online Planner & Organizer",
